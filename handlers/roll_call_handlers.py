88ac33055e8752e8c35435381d3e57dfae0c311e
Timothy Cloninger <timothy.cloninger@life.church>
2018-09-09 18:20:55 -0500

    Add RollCall handlers

---
+++ handlers/roll_call_handlers.py
@@ -0,0 +1,75 @@
+from ask_sdk_core.dispatch_components import AbstractRequestHandler
+from ask_sdk_core.utils import is_intent_name, is_request_type
+
+from config import settings
+from utils.rollcall import RollCall
+
+
+class GameEventHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        session_attrs = handler_input.attributes_manager.session_attributes
+        return (
+            is_request_type("GameEngine.InputHandlerEvent")(handler_input) and
+            session_attrs['STATE'] == settings.STATES['rollcall']
+        )
+
+    def handle(self, handler_input):
+        print('GameEventHandler ----------------------')
+        request_env = handler_input.request_envelope
+        attrs_manager = handler_input.attributes_manager
+        request_attrs = attrs_manager.request_attributes
+        session_attrs = attrs_manager.session_attributes
+
+        if request_env.request.originating_request_id == session_attrs['input_handler_id']:
+            print(
+                f"Global.GameEngineInputHandler: stale input event received -> "
+                f"received event from {request_env.request.originating_request_id} "
+                f" (was expecting {session_attrs['input_handler_id']})"
+            )
+            request_attrs['open_microphone'] = False
+            return handler_input.response_builder.response
+
+        RollCall.handle_events(handler_input, request_env.request.events)
+        return handler_input.response_builder.response
+
+
+class NoHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        session_attrs = handler_input.attributes_manager.session_attributes
+        return (
+            is_request_type("IntentRequest")(handler_input) and
+            is_intent_name("AMAZON.NoIntent")(handler_input) and
+            session_attrs['STATE'] == settings.STATES['rollcall']
+        )
+
+    def handle(self, handler_input):
+        print('start_handlers.NoHandler ----------------------')
+        request_attrs = handler_input.attributes_manager.request_attributes
+
+        # let responseMessage = ctx.t('GOOD_BYE');
+        messages = {
+            'output_speech': 'rollcall.no',
+        }
+        request_attrs['output_speech'].append(messages['output_speech'])
+        request_attrs['open_microphone'] = False
+        handler_input.response_builder.set_should_end_session(True)
+
+        return handler_input.response_builder.response
+
+
+class YesHandler(AbstractRequestHandler):
+    def can_handle(self, handler_input):
+        session_attrs = handler_input.attributes_manager.session_attributes
+        return (
+            is_request_type("IntentRequest")(handler_input) and
+            is_intent_name("AMAZON.YesIntent")(handler_input) and
+            session_attrs['STATE'] == settings.STATES['rollcall']
+        )
+
+    def handle(self, handler_input):
+        print('RollCall.YesHandler ----------------------')
+        session_attrs = handler_input.attributes_manager.session_attributes
+
+        RollCall.start(handler_input, False, session_attrs['player_count'])
+
+        return handler_input.response_builder.response
