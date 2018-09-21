8fdf2da0f7c9fb37c65847e50d2839b53f5482d5
Timothy Cloninger <timothy.cloninger@life.church>
2018-09-20 21:46:52 -0500

    Add questions

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,18 +1,22 @@
 import gettext
 
-from config import settings
-
-i18n = gettext.translation('messages', localedir='locale', languages=['en_US'])
+from config import questions, settings
 
 
 def _(key, data={}):
-    types = ['output_speech', 'reprompt', 'display_title', 'display_text']
-    messages = {}
+    if key == 'QUESTIONS':
+        return questions.en_US
 
-    for type_ in types:
-        expanded_key = f"{key}_{type_}"
-        output = i18n.gettext(expanded_key)
-        if output != expanded_key:
-            messages[type_] = output.format(**{**settings.GAME_OPTIONS, **data})
+    else:
+        i18n = gettext.translation('messages', localedir='locale', languages=['en_US'])
 
-    return messages
+        types = ['output_speech', 'reprompt', 'display_title', 'display_text']
+        messages = {}
+
+        for type_ in types:
+            expanded_key = f"{key}_{type_}"
+            output = i18n.gettext(expanded_key)
+            if output != expanded_key:
+                messages[type_] = output.format(**{**settings.GAME_OPTIONS, **data})
+
+        return messages
